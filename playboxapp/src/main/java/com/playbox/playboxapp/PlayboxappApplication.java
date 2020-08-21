cc5f9a6b49ef33a6b59f5baad091110b1c9eac61
Kansaji <lakruwan45@gmail.com>
2020-08-21 18:48:32 +0530

    Created new spring boot project.Updated application properties file

---
+++ playboxapp/src/main/java/com/playbox/playboxapp/PlayboxappApplication.java
@@ -0,0 +1,13 @@
+package com.playbox.playboxapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayboxappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayboxappApplication.class, args);
+	}
+
+}
---
+++ playboxapp/src/test/java/com/playbox/playboxapp/PlayboxappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.playbox.playboxapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayboxappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
